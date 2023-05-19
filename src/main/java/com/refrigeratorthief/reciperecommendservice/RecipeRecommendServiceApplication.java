035d8ca9ec7ea4afeb234df6354bc1163935f575
hyeonhee <dlgusgml1314@naver.com>
2023-05-19 21:17:51 +0900

    Initial commit

---
+++ src/main/java/com/refrigeratorthief/reciperecommendservice/RecipeRecommendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.refrigeratorthief.reciperecommendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRecommendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeRecommendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/refrigeratorthief/reciperecommendservice/RecipeRecommendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refrigeratorthief.reciperecommendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
